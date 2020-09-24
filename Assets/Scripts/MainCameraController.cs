7c2592963090565ab53b64c046129606ffb31963
Prokop Randáček <prokop@randacek.dev>
2020-09-24 20:27:07 +0200

    Portal rendering

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    private PortalCameraController[] _portals;
+    void Awake()
+    {
+        _portals = FindObjectsOfType<PortalCameraController>();
+    }
+
+    // Update is called once per frame
+    void OnPreCull()
+    {
+        foreach (PortalCameraController portal in _portals)
+        {
+            portal.Render();
+        }
+    }
+}
--- Assets/Scripts/PortalCameraController.cs
+++ Assets/Scripts/PortalCameraController.cs
@@ -2,19 +2,61 @@
 
 public class PortalCameraController : MonoBehaviour
 {
-    private Transform _playerCam;
-    public  Transform mePortal;
-    public  Transform otherPortal;
-    
-    void Start()
+    public PortalCameraController otherPortal;
+    public MeshRenderer           screen;
+    public Vector3                portalOffset;
+    public GameObject             mePortal;
+
+    private Camera        _portalCam;
+    private Transform     _playerPos;
+    private Transform     _mePortalPos;
+    private Transform     _otherPortalPos;
+    private GameObject    _otherPortalScreen;
+    private RenderTexture _viewTexture;
+
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+    void Awake()
     {
-        _playerCam = Camera.main.GetComponent<Transform>();
+        // ReSharper disable once PossibleNullReferenceException
+        _playerPos      = Camera.main.GetComponent<Transform>();
+        _portalCam      = GetComponentInChildren<Camera>();
+        _mePortalPos    = transform.parent;
+        _otherPortalPos = otherPortal.mePortal.transform;
+        mePortal        = _mePortalPos.gameObject;
+
+        //portalOffset = transform.position - _otherPortalPos.transform.position; // Vector pointing from the other portal to me
+        
+        _portalCam.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateViewTexture()
     {
-        transform.position = otherPortal.position - (mePortal.position - _playerCam.position);
-        transform.rotation = _playerCam.rotation;
+        if (_viewTexture == null || _viewTexture.width != Screen.width || _viewTexture.height != Screen.height)
+        {
+            if (_viewTexture != null)
+                _viewTexture.Release();
+            _viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+            _portalCam.targetTexture = _viewTexture;
+            otherPortal.screen.material.SetTexture(MainTex, _viewTexture);
+        }
+    }
+    
+    // Camera position and rotation calculation
+    private void Move()
+    {
+        transform.position = _mePortalPos.position + (_playerPos.position - _otherPortalPos.position);
+        transform.rotation = _playerPos.rotation; //TODO this might not be right when multiple portals are behind with different rotations?
+    }
+    
+    public void Render()
+    {
+        screen.enabled = false;
+        
+        CreateViewTexture();
+        Move(); //FIXME
+        _portalCam.Render();
+
+        screen.enabled = true;
     }
 }
