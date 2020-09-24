e86e2c1b92ebf5849010816a6ca047095a9eb069
Prokop Randáček <prokop@randacek.dev>
2020-09-24 17:10:34 +0200

    Portal camera movement

---
+++ Assets/Scripts/PortalCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PortalCameraController : MonoBehaviour
+{
+    private Transform _playerCam;
+    public  Transform mePortal;
+    public  Transform otherPortal;
+    
+    void Start()
+    {
+        _playerCam = Camera.main.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = otherPortal.position - (mePortal.position - _playerCam.position);
+        transform.rotation = _playerCam.rotation;
+    }
+}
